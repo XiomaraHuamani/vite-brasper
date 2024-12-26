640ce8cd7bf40d20df8d74a21d729e7b315ea616
XiomaraHuamani <xiomara.huamani.c@gmail.com>
2024-12-26 12:32:31 -0500

    establesimiento de la tipografia

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom'; // Importa BrowserRouter
-import './style/index.css';
+import './style/fontstyle.css';
 import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
